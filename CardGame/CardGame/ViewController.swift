ab01057eda9b47479f9fedb9000a9869ba05c2a7
RyanGreenburg <>
2020-12-14 20:12:33 -0700

    creates starter branch with assets

---
+++ CardGame/CardGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardGame
+//
+//  Created by RYAN GREENBURG on 12/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
